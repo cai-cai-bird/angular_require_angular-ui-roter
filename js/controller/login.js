472dfecbce04b81a24b5828df0fa65881f2de6f3
liuxin <liuxin@newborn-town.com>
2016-08-04 09:56:27 +0800

    test

--- js/controller/login.js
+++ js/controller/login.js
@@ -1,8 +1,8 @@
 /**
  * Created by sa on 16-8-3.
  */
-define(["../module/module"],function (app) {
-
+define(["../module/module","../service/testService"],function (app,aa) {
+console.log(aa)
     app.controller("loginController",["$scope",function ($scope,testService) {
         console.log(testService)
     }])
--- js/service/testService.js
+++ js/service/testService.js
@@ -1,8 +1,8 @@
 /**
  * Created by liuxin on 2016/8/3 0003.
  */
-define(["../serviceModule"],function (serviceModule) {
-    return serviceModule.service('testService',  ['$http',function ($http) {
+define(["./serviceMoudle"],function (serviceModule) {
+    return serviceModule.factory('testService',  ['$http',function ($http) {
         return [1,2,3,4,5];
    }]);
 });
